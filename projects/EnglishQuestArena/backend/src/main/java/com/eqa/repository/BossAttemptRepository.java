fc86de7c503fa0ed4df9541a10a1fc42e80ef2c8
Felix-gg-cloud <guozg@cn.ibm.com>
2026-04-03 16:05:27 +0800

    Delete projects/EnglishQuestArena directory

--- projects/EnglishQuestArena/backend/src/main/java/com/eqa/repository/BossAttemptRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eqa.repository;
-
-import com.eqa.entity.BossAttempt;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BossAttemptRepository extends JpaRepository<BossAttempt, Long> {
-}
