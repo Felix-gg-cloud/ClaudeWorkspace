717cc8e532f149950fbf3f9741a4313cce7cd8bd
Felix-gg-cloud <guozg@cn.ibm.com>
2026-04-03 16:05:13 +0800

    Delete projects/LingoBuddy directory

--- projects/LingoBuddy/backend/src/main/java/com/lingobuddy/entity/TaskType.java
+++
@@ -1,7 +0,0 @@
-package com.lingobuddy.entity;
-
-public enum TaskType {
-    VOCAB_CARD,
-    QUIZ_SINGLE,
-    SPELLING
-}
--- projects/LingoBuddy/backend/src/main/java/com/lingobuddy/repository/SeedImportLogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lingobuddy.repository;
-
-import com.lingobuddy.entity.SeedImportLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeedImportLogRepository extends JpaRepository<SeedImportLog, Long> {
-    boolean existsByFileName(String fileName);
-}
